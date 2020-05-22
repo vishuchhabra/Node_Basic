ae4a92a8a02afee11d10099cad853fb86b667872
vishuchhabra <vishuchhabra1016@gmail.com>
2020-05-22 22:36:53 +0530

    This is another update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http  = require('http')
 const port  = 4000
 const app = require('./app')
-const server = http.createServer()
+const server = http.createServer(app)
 server.listen(port,()=>
 {
     console.log(`server has been started on ${port} `)
